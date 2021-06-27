e53de15e400ebb81051aca864877c396fe46978c
Plat-26 <lolade.badmus97@gmail.com>
2021-06-27 17:05:41 +0100

    configured email validator

--- src/main/java/com/loladebadmus/simplecrudapp/users/registration/EmailValidator.java
+++ src/main/java/com/loladebadmus/simplecrudapp/users/registration/EmailValidator.java
@@ -3,12 +3,15 @@ package com.loladebadmus.simplecrudapp.users.registration;
 import org.springframework.stereotype.Service;
 
 import java.util.function.Predicate;
+import java.util.regex.Pattern;
 
 @Service
 public class EmailValidator implements Predicate<String> {
+
+    public final static Pattern EMAIL_REGEX = Pattern.compile("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", Pattern.CASE_INSENSITIVE);
+
     @Override
-    public boolean test(String s) {
-        //todo: regex to validate email
-        return true;
+    public boolean test(String email) {
+        return EMAIL_REGEX.matcher(email).matches();
     }
 }
